a7a5ab0758152fd6f8043ebff30576faecab1827
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-11-16 14:15:55 -0800

    Naive Bayes Calc

--- bayes-classification.cpp
+++ bayes-classification.cpp
@@ -65,19 +65,109 @@ class ExactBayesClassification : public Classifier {
 };
 
 
+/* *
+   * For each classification compute P(Query maps to classification | queryRec vector vals)
+   * Expand this to:
+   * P(queryRec[0] | classification) * P(queryRec[1] | classification)....*P(queryRec[i] | classification) / P(vector[0])*...*P(vector[i])
+   * 
+   * Helpful equations:
+   * P (A | B) = P ( A & B) / P(B)
+   * P (x) = number of times x occurs / total occurences
+   * */
 class NaiveBayesClassification : public Classifier {
   public:
 
+  /**
+   * For each record we can create lookup data structures.
+   * We should store the following :
+   * - probabilities of each classification
+   * - A vector of the training records for in time access
+   * - A set of all classifications
+   * */
   void Train(const std::vector<Record>& _records) {
+    records = _records;
+    
+    for (auto const& record : records) {
+      auto classification = record.classification;
+      
+      classes.insert(classification);
 
+      if (classificationProbabilites.find(classification) == classificationProbabilites.end()) {
+        classificationProbabilites[classification] = 0;
+      }
+      classificationProbabilites[classification]++;
+    }
+    for (auto it: classificationProbabilites) {
+      classificationProbabilites[it.first] = it.second / (double) records.size();
+    }
   }
   
   std::unordered_map<std::string, double> Classify(const std::vector<bool>& queryRec) {
+    std::unordered_map<std::string, double> result;
 
+    for (auto classification: classes) {
+      // find the probability of this classification and add it to our result map
+      result[classification] = ProbabilityOfClassificationGivenQuery(classification, queryRec);
+    }
+
+    return result;
   }
 
   private:
+  
+  std::set<std::string> classes;
+  std::vector<Record> records;
+  std::unordered_map<std::string, double> classificationProbabilites;
 
+  /**
+   * P(classification) * [P(queryRec[0] | classification) *..* P(queryRec[i] | classification)
+   *                      ------------------------------------------------------------------
+   *                                  P(queryRec[0]) *..* P(queryRec[i])
+   * 
+   * => P(classification) * P(queryyRec[0] && classification) / P(classification) *** P(queryyRec[i] && classification) / P(classification)
+   *                        ----------------------------------------------------------------------------------------------------------------
+   *                        P(queryRec[0]) *..* P(queryRec[i])
+   *
+   * => P(classification) * (#queryRec[0]&&classification / #classification) ** (#queryRec[i]&&classification / #classification)
+   *                        -----------------------------------------------------------------------------------------------------
+   *                               (#queryRec[0]/#recs) *..* (#queryRec[i]/#recs) 
+   * 
+   * Helper Equation Expansion:
+   * P(queryRec[i] | classification) = P(classificaton && queryRec[i]) / P (classification)
+   * 
+   * P(classificaton && queryRec[i]) = ....
+   * */
+  double ProbabilityOfClassificationGivenQuery(std::string& classification, const std::vector<bool>& queryRec) {
+    double denominator = 1;
+    double numerator = 1;
+    double classificationProb = classificationProbabilites.at(classification);
+
+    for (int i = 0; i < queryRec.size(); i++) {
+      double queryRecAndClassificationCount = 0;
+      double classificationCount = 0;
+      double recQueryCatCount = 0;
+
+      for (auto rec: records) {
+
+        if (rec.categoricalVariables.at(i) == queryRec.at(i)) {
+          recQueryCatCount++;
+        }
+
+        if (rec.classification == classification && rec.categoricalVariables.at(i) == queryRec.at(i)) {
+          queryRecAndClassificationCount++;
+        }
+
+        if (rec.classification == classification) {
+          classificationCount++;
+        }
+      }
+      
+      numerator *= queryRecAndClassificationCount/classificationCount;
+      denominator *= recQueryCatCount/records.size();
+    }
+
+    return classificationProb * (numerator/denominator);
+  }
 };
 
 
@@ -103,13 +193,27 @@ int main() {
 
   ExactBayesClassification ebc;
 
+  NaiveBayesClassification nbc;
+
   ebc.Train(recs);
+  nbc.Train(recs);
 
   std::vector<bool> query = {true, false, true, true};
 
-  auto probs = ebc.Classify(query);
+  auto ebcProbs = ebc.Classify(query);
 
-  for (auto res: probs) {
+  std::cout << "####### EXACT BAYES CLASSIFICATION #######" <<std::endl;
+
+  for (auto res: ebcProbs) {
+    std::cout << res.first << ": " << res.second << "\n";
+  }
+
+  std::cout << "####### NAIVE BAYES CLASSIFICATION #######" <<std::endl;
+
+
+  auto nbcProbs = nbc.Classify(query);
+
+  for (auto res: nbcProbs) {
     std::cout << res.first << ": " << res.second << "\n";
   }
 }
